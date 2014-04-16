ba88d89055afd09887532e9412b4108d11bb75d2
root <root@backpacking.(none)>
2014-04-16 15:15:17 -0600

    rob and i at home

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 //NAVBAR - load navbar tpl and assign corrent active class
 $('#navbar').load('tpl/header_nav.tpl.html',function() {
-    var currentPage = document.location.href.match(/[^\/]+$/)[0];
+    var currentPage = document.location.href.match(/[^\/]+$/);
     $('.nav li').each(function() {
-        if ($(this).children('a').attr('href') == currentPage) $(this).addClass('active');
+	if (currentPage == null && $(this).children('a').attr('href') == 'index.html') $(this).addClass('active');
+	else if ($(this).children('a').attr('href') == currentPage) $(this).addClass('active');
     });
 });
