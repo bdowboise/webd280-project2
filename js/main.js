e8740ef4c68cd2fd2e676388a7acb967eedc3ec1
bdowboise <bdowboise@gmail.com>
2014-04-17 15:25:21 -0600

    updates to gallery and backToTop button
    -added magnific popup to gallery
    -add back to top button in main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ $('#navbar').load('tpl/header_nav.tpl.html',function() {
 	else if ($(this).children('a').attr('href') == currentPage) $(this).addClass('active');
     });
 });
+//back to top button
+$('<button id="backToTop" type="button" class="btn btn-sm"><span class="glyphicon glyphicon-chevron-up"></span></button>')
+    .appendTo('body')
+    .addClass('hide');
+$(window).scroll(function() {
+    var top = $(window).scrollTop();
+    if (top >= 300) {
+        $('#backToTop').addClass('show');
+    }
+    else {
+        $('#backToTop').removeClass('show');
+    }
+});
+$('#backToTop').click(function() {
+   $('html,body').animate({ scrollTop: "0px" });
+});
