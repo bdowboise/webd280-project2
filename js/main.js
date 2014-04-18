0fe084cae7698596a1144a30b40e3575da1e9945
bdowboise <bdowboise@gmail.com>
2014-04-18 10:02:44 -0600

    adds mobile responsive menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //NAVBAR - load navbar tpl and assign corrent active class
-$('#navbar').load('tpl/header_nav.tpl.html',function() {
+$('body :after').load('tpl/header_nav.tpl.html',function() {
     var currentPage = document.location.href.match(/[^\/]+$/);
     $('.nav li').each(function() {
 	if (currentPage == null && $(this).children('a').attr('href') == 'index.html') $(this).addClass('active');
